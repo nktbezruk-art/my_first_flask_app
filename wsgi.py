e61936fc33f94425f958a7b42859d6fd452c5b84
nktbezruk-art <nktezruk@gmail.com>
2025-10-20 19:06:40 +0300

    Add wsgi.py for deployment

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Добавляем путь к проекту
+path = os.path.dirname(os.path.abspath(__file__))
+if path not in sys.path:
+    sys.path.append(path)
+
+# Импортируем приложение
+from app import app as application
